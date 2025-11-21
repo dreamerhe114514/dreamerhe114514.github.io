d6baa08fba34c413630b77739b9b8924aad48173
dreamerhe114514 <dreamerhe7747@hotmail.com>
2025-11-21 16:53:43 +0800

    Site updated: 2025-11-21 16:53:15

--- mogaijs/dingyue.js
+++ mogaijs/dingyue.js
@@ -1,14 +1,1 @@
-var icat = {
-    submitInfo: function() {
-        var submitBox = document.querySelector('.submit-box');
-        var followElement = document.querySelector('#follow');
-
-        if (submitBox) {
-            submitBox.classList.toggle('display');
-        }
-
-        if (followElement) {
-            followElement.classList.toggle('display');
-        }
-    }
- }
+var icat={submitInfo:function(){var t=document.querySelector(".submit-box"),o=document.querySelector("#follow");t&&t.classList.toggle("display"),o&&o.classList.toggle("display")}};
